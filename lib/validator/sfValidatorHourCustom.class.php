672e65094edfa4b45dbe6de2188f1a7ca4e86040
Vincent CHALAMON <vincentchalamon@gmail.com>
2011-06-22 13:51:31 +0200

    Debug validator hour custom

--- lib/validator/sfValidatorHourCustom.class.php
+++ lib/validator/sfValidatorHourCustom.class.php
@@ -5,7 +5,7 @@
 class sfValidatorDateCustom extends sfValidatorTime
 {
   protected function doClean($value) {
-    if(!preg_match('/(\d{2})h(\d{2})/i')) {
+    if(!preg_match('/(\d{2})h(\d{2})/i', $value)) {
       throw new sfValidatorError($this, 'invalid', array('value' => $value));
     }
     return parent::doClean(array('hour' => preg_replace('/(\d{2})h(\d{2})/i', '$1', $value), 'minute' => preg_replace('/(\d{2})h(\d{2})/i', '$2', $value)));
