23f7f8e58a9ea5efdd0c60d79e79b155629fff32
Vincent CHALAMON <vincentchalamon@gmail.com>
2011-08-10 15:16:34 +0200

    Update widget timestamp

--- lib/widget/sfWidgetFormTimestamp.class.php
+++ lib/widget/sfWidgetFormTimestamp.class.php
@@ -2,15 +2,10 @@
 
 class sfWidgetFormTimestamp extends sfWidgetFormDateJQueryUI
 {
-  /**
-   * Prepare widget
-   * 
-   * @param array $options
-   * @param array $attributes
-   */
   protected function configure($options = array(), $attributes = array()) {
     parent::configure($options, $attributes);
-    $this->addOption('widget');
+    $this->addOption("stepHour", 1);
+    $this->addOption("stepMinute", 1);
   }
 
   /**
@@ -25,18 +20,56 @@ class sfWidgetFormTimestamp extends sfWidgetFormDateJQueryUI
    */
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
-    $widget = $this->getOption('widget') ? $this->getOption('widget') : new sfWidgetFormInputText();
-    return "<div class='sf_widget_timestamp'>".parent::render($name."[date]", $value ? date('Y-m-d', strtotime($value)) : null, $attributes, $errors).$widget->render($name."[hour]", $value ? date('H\hi', strtotime($value)) : null, $widget->getAttributes(), $errors)."</div><div style='clear: both;'></div>";
+    $pattern = "/^(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})$/i";
+    return $this->renderTag('input', array_merge(array('type' => $this->getOption('type'), 'name' => $name, 'value' => preg_replace($pattern, "$3/$2/$1 $4H$5", $value)), $attributes)).sprintf(<<<EOF
+<script type="text/javascript">
+$(function() {
+  %s
+  $("#%s").datetimepicker({
+    hour: %s,
+    minute: %s,
+    timeFormat : 'hhHmm',
+    stepHour: %s,
+    stepMinute: %s,
+    hourGrid: 4,
+    minuteGrid: 15,
+    regional : '%s',
+    changeMonth : %s,
+    changeYear : %s,
+    numberOfMonths : %s,
+    dateFormat: 'dd/mm/yy',
+    showButtonPanel : %s,
+    minDate: %s
+  });
+});
+</script>
+EOF
+            , $this->getOption('culture') != "en" ? "$.timepicker.regional['".$this->getOption('culture')."'];\n  $.datepicker.regional['".$this->getOption('culture')."'];" : null
+            , $this->generateId($name, $value)
+            , preg_match($pattern, $value) ? preg_replace($pattern, "$4", $value) : 0
+            , preg_match($pattern, $value) ? preg_replace($pattern, "$5", $value) : 0
+            , $this->getOption('stepHour')
+            , $this->getOption('stepMinute')
+            , $this->getOption('culture')
+            , $this->getOption("change_month") ? "true" : "false"
+            , $this->getOption("change_year") ? "true" : "false"
+            , $this->getOption("number_of_months")
+            , $this->getOption("show_button_panel") ? "true" : "false"
+            , $this->getOption("show_previous_dates") ? "null" : "new Date()"
+            );
   }
 
   public function getJavaScripts() {
-    $widget = $this->getOption('widget') ? $this->getOption('widget') : new sfWidgetFormInputText($this->getOption('options'), $this->getOption('attributes'));
-    return array_merge(parent::getJavaScripts(), $widget->getJavaScripts());
+    $javascripts = array("/sfEPFactoryFormPlugin/timepicker/timepicker.jQuery.js");
+    $culture = $this->getOption('culture');
+    if($culture != 'en') {
+      $javascripts[] = "/sfEPFactoryFormPlugin/jqueryui/i18n/ui.timepicker-$culture.js";
+    }
+    return array_merge(parent::getJavaScripts(), $javascripts);
   }
 
   public function getStylesheets() {
-    $widget = $this->getOption('widget') ? $this->getOption('widget') : new sfWidgetFormInputText($this->getOption('options'), $this->getOption('attributes'));
-    return array_merge(parent::getStylesheets(), $widget->getStylesheets());
+    return array_merge(parent::getStylesheets(), array("/sfEPFactoryFormPlugin/timepicker/timepicker.css" => "screen"));
   }
 
 }
---
+++ web/jqueryui/i18n/ui.timepicker-fr.js
@@ -0,0 +1,15 @@
+﻿/* French initialisation for the jQuery UI date picker plugin. */
+/* Written by Keith Wood (kbwood@virginbroadband.com.au) and Stéphane Nahmani (sholby@sholby.net). */
+jQuery(function($){
+	$.timepicker.regional['fr'] = {
+      timeOnlyTitle: "Saisie d'un temps",
+      timeText: 'Temps',
+      hourText: 'Heures',
+      minuteText: 'Minutes',
+      secondText: 'Secondes',
+      currentText: 'Maintenant',
+      closeText: 'Fermer',
+      ampm: false
+    };
+	$.timepicker.setDefaults($.timepicker.regional['fr']);
+});
